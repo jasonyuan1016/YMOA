170fd71c907200842229cc0959078368383c8fe0
JY <jason0824@foxmail.com>
2019-05-14 17:15:48 +0800

    nuget整理

---
+++ YMOA.UnitTest/SystemTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YMOA.UnitTest
+{
+    [TestClass]
+    public class SystemTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
