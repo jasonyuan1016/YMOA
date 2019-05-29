5d462e9eac3f51af249815c19988ce9ab47dfcf3
the-iceberg <2608428799@qq.com>
2019-05-29 11:41:11 +0800

    提交tbTask，tbAccessory实体类

---
+++ YMOA.Model/AccessoryEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMOA.Model
+{
+
+    /// <summary>
+    ///  附件实体
+    ///  创建者: zxy
+    ///  创建时间: 2019年5月29日
+    /// </summary>
+    public class AccessoryEntity
+    {
+        /// <summary>
+        ///  附件编号
+        /// </summary>
+        public string ID { get; set; }
+        /// <summary>
+        ///  标题
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        ///  任务编号
+        /// </summary>
+        public string TaskId { get; set; }
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string AccessoryUrl { get; set; }
+    }
+}
